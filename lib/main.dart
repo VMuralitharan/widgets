800490de04904f485a5696add1be239344254de0
VMuralitharan <vijayakumarmuralitharan@gmail.com>
2025-07-16 20:29:50 +0530

    "undo Changes"

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class secondappstate extends State<secondapp> {
                   print(displayText);
                 });
               },
-              child: Tex("click me"),
+              child: Text("click me"),
             ),
           ],
         ),
@@ -38,3 +38,6 @@ class secondappstate extends State<secondapp> {
     );
   }
 }
+
+
+
